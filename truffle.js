3d5f0932b6acd23ab903434c9345a12ed9fb148f
Laura Weindorf <lauraweindorf@gmail.com>
2019-04-21 16:22:48 -0400

    cleaned up truffle config

--- truffle.js
+++ truffle.js
@@ -1,6 +1,3 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-// truffle ganache 7545
-var mnemonic = "urge stadium swap mind air busy return door infant reason industry garment";
 
 module.exports = {
   networks: {
